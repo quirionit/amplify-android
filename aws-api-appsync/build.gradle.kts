a72e6afdab5815c35f69bb275d36188b50ca007a
Daniil Belevtsev <daniil.belevtsev@quirion.de>
2024-08-30 19:30:38 +0200

    add jitpack config

--- aws-api-appsync/build.gradle.kts
+++ aws-api-appsync/build.gradle.kts
@@ -18,7 +18,7 @@ plugins {
     id("kotlin-android")
 }
 
-apply(from = rootProject.file("configuration/checkstyle.gradle"))
+//apply(from = rootProject.file("configuration/checkstyle.gradle"))
 apply(from = rootProject.file("configuration/publishing.gradle"))
 
 group = properties["POM_GROUP"].toString()
